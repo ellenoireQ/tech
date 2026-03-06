94f9af251125603a5f7a7461845e97880a935912
ellenoireQ <ketchupnaiso@gmail.com>
2026-03-06 10:48:19 +0700

    docs(backend): Commenting user model

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,5 +1,14 @@
 from pydantic import BaseModel
 
+
+# User Login Model
+#
+# example:
+#
+# UserLogin(
+# email = "x@x.com"
+# password = "x"
+# )
 class UserLogin(BaseModel):
     email: str
     password: str
