d4bc90cff2a047afd063e2f990038d3214d9f69a
ellenoireQ <ketchupnaiso@gmail.com>
2026-03-06 19:15:24 +0700

    fix(imports): remove redundant import of joblib from sklearn.utils.parallel

--- ml/utils/train.py
+++ ml/utils/train.py
@@ -1,8 +1,8 @@
+import joblib
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.utils.parallel import joblib
 
 
 def train_model(d: dict):
