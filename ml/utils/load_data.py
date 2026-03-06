9f4686548f72e0fdb3dcc6de101cc9b4b8e78ddf
ellenoireQ <ketchupnaiso@gmail.com>
2026-03-06 13:44:05 +0700

    docs(backend): Commenting load__data

--- ml/utils/load_data.py
+++ ml/utils/load_data.py
@@ -4,6 +4,15 @@ import pandas as pd
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
+# load_data
+#
+# Description:
+# Loads data from the sales_data.csv file and returns it in JSON format.
+# Once loaded, the data can be used for prediction.
+#
+# example;
+# load_data(10): meaning is expanding value 10
+#
 def load_data(expand: int):
     cols = [
         "product_id",
