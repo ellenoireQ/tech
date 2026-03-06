27a3cee15773fbc00a285765fa302a7bf24ecfb4
ellenoireQ <ketchupnaiso@gmail.com>
2026-03-06 11:15:00 +0700

    feat(ml): implement load_data function and add unit testing setup

--- ml/main.py
+++ ml/main.py
@@ -1,1 +1,15 @@
-#
+# ------------
+# Unit Testing
+# ------------
+
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from ml.utils.load_data import load_data
+
+# load_data()
+# utils/load_data.py
+d = load_data()
+print(d)  # returning all sales_data (in load_data() function returning 50)
---
+++ ml/utils/load_data.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+def load_data():
+    cols = [
+        "product_id",
+        "product_name",
+        "jumlah_penjualan",
+        "harga",
+        "diskon",
+        "status",
+    ]
+
+    sales_data = pd.read_csv(
+        os.path.join(BASE_DIR, "data", "sales_data.csv"), index_col="product_id", usecols=cols
+    )
+
+    return sales_data.head(50)
